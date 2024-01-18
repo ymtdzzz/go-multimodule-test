f443a9b122b4a8129248d60a8c764211d1465bb1
Yosuke Matsuda <yosuke.matsuda@makuake.co.jp>
2024-01-18 18:09:29 +0900

    tmp

--- api/main.go
+++ api/main.go
@@ -6,10 +6,6 @@ import (
 	"github.com/ymtdzzz/go-multimodule-test/internal"
 )
 
-func main() {
-	fmt.Println(internal.CreateMessage())
-}
-
 func GetMessage() string {
 	return fmt.Sprintln(internal.CreateMessage())
 }
